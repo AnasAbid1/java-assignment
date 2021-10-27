b278ded67ff3b52c7bd9e789e63c58b0c2f475d7
Anas Abid <abid.anas3@gmail.com>
2021-10-27 14:53:39 -0700

    Java script assignment

---
+++ app.js
@@ -0,0 +1,438 @@
+                                //Chapter 1//
+//1)
+//alert("HELLO CLASS")
+
+//2)
+//alert("Error!Please Enter a Valid password.")
+
+//3)
+//alert("Welcome to JS land \n Happy coding!")
+
+//4)
+//alert("Welcome to JS land")
+//alert("Happy coding..!")
+
+//5)
+//alert("Hello..!I can run JS through my web browser's console")
+
+//6)Done
+
+//7)Done
+
+                    //chapter 2//
+
+//1)
+//var username ="....."
+
+//2)
+//var myName = "Anas Abid"
+//alert(myName)
+
+//3)
+//var message = "HELLO WORLD"
+//alert(message)
+
+//4)
+//var userName = "Anas Abid"
+//alert(userName)
+//var age = "18 years old"
+//alert(age)
+//var message = "Certified Mobile Application Development"
+//alert(message)
+
+//5)
+//var message = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+//alert(message)
+
+//6)
+//var email = "abid.anas3@gmail.com"
+//alert("My Email Address is : " + email)
+
+//7)
+//var book =  "A smarter way to learn JavaScript"
+//alert(book)
+
+//8)
+//var message = "Yeah! I can write HTML content through JavaScript"
+//document.write(message)
+
+//9)
+//var message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+//alert(message)
+//document.write(message)
+
+                            //Chapter 3//
+
+//1)
+//var userAge = "I am 18 years old"
+//alert(userAge)
+
+//2)
+//var numofVisits = "you have visited this page 2 times"
+//alert(numofVisits)
+
+//3)
+//var birthYear = 2003
+//alert("My birth year is "+ birthYear)
+//document.write("My Birth Year is "+ birthYear)
+//document.write("<br>Data type of my declared variable is number")
+
+//4)
+//var visitorName = "John Doe"
+//var productTitle = "T-Shirt(s)"
+//var quantity = 5
+//document.write(visitorName + " ordered " + quantity + productTitle + " on XYZ Clothing store ")
+
+                            //Chapter 4//
+
+//1)
+//var number , name , age = " 5 Anas 18 " 
+
+//2)
+//legal//
+//var userName ="Anas Abid"
+//var userAge = 18
+//var userId = 20090100125
+//var userCnic = 42201-1712580-3
+//var userInfo = "xyz....."
+//Illegal//
+//var username ="Anas Abid"
+//var userage = 78
+//var userid = 2656235
+//var user cnic = 654625956
+//var userinfo ="njkgjhnkhuiyfghvj"
+
+//3)
+//a)
+//document.write("<h1>Rules for naming JS variables</h1")
+//b)
+//document.write(" Variable names can only contain , numbers , $ or ____ . For Example : $my_1stvariable")
+//c)
+//document.write("Variable must began with a letter, $ or __ . For example : $name,__name or name")
+//document.write("Variable names are Case Sensitive")
+//document.write("Variable names should not be JS KeywordsS")
+                            //Chappter 5//
+
+//1)
+//var num1 = 3;
+//var num2 = 5;
+
+//var add = num1 + num2
+
+//alert(add)
+//document.write("sum of "+ num1 +" and " + num2 + " is " + add )
+
+//2)
+//Subtraction//
+//var num1 = 3;
+//var num2 = 5;
+
+//var substract = num1 - num2
+
+//alert(substract)
+//document.write("sum of "+ num1 +" and " + num2 + " is " + subtract )
+
+//Multiplication
+//var num1 = 3;
+//var num2 = 5;
+
+//var multiply = num1 * num2
+//alert(multiply)
+//document.write("multiple of "+ num1 +" and " + num2 + " is " + multiple )
+//Division
+//var num1 = 3;
+//var num2 = 5;
+
+//var divide = num1 / num2
+
+//alert(divide)
+//document.write("division of "+ num1 +" and " + num2 + " is " + divide )
+
+//3)
+//var mathExpression = "??"
+//document.write("Value after variable declaration is:" + mathExpression)
+//var mathExpression = 5
+//document.write("Initial value : " + mathExpression)
+//var mathExpression = ++5
+//document.write("Value after increment is " + mathExpression)
+//var addition = mathExpression+7
+//document.write("Value after addition is"+ addition)
+//var decrement = --mathExpression
+//document.write("Value after decrement is" + decrement)
+//var remainder = decrement / 3
+//document.write("Value of the remainder is" + remainder)
+
+//4)
+//var ticketPrice = 600*5
+//document.write("Cost of buying 5 tickets is " + ticketPrice)
+
+//5)
+//var number = 4
+//document.write("4*1=" + number*1) 
+//document.write("<br>4*2=" + number*2)
+//document.write("<br>4*3=" + number*3)
+//document.write("<br>4*4=" + number*4)
+//document.write("<br>4*5=" + number*5) 
+//document.write("<br>4*6=" + number*6) 
+//document.write("<br>4*7=" + number*7) 
+//document.write("<br>4*8=" + number*8) 
+//document.write("<br>4*9=" + number*9) 
+//document.write("<br>4*10=" + number*10) 
+//document.write("<br>4*11=" + number*11) 
+
+//6)
+//var celsiustemperature = 25
+//var conversion = (25*9/5)=32
+//document.write(celsiustemperature + "is" + conversion)
+// var farenheitTemperature = 70
+// var convert = (70-32)*5/9
+//document.write(farenheitTemperature + "is" + convert)
+
+//7)
+//var item1 = 650
+//var item2 = 100
+//var quantityItem1 = 3
+//var quantityItem2 = 7
+//var shippingCharges = 100
+//var total = item1*quantityItem1 + item2*quantityItem2 + shippingCharges
+//document.write("Price of item 1 is "+item1)
+//document.write("<br>Quantity if item 1 is " + quantityItem1)
+//document.write("<br>Price of item 2 is " + item2)
+//document.write("<br>Quantity if item 2 is " + quantityItem2)
+//document.write("<br>Shipping Charges " + shippingCharges)
+//document.write("<br>Total cost of your order is " + total)
+
+//8)
+
+//var obtainedMarks = 350
+//document.write("<br>Obtained Marks:"+obtainedMarks)
+
+//var totalMarks = 400
+//document.write("<br>Total Marks:"+400)
+
+//var totalPercentage = (obtainedMarks*100/totalMarks)
+//document.write("<br>Percentage:"+totalPercentage)
+
+//9)
+//var US$ =  104.80
+//var saudiRiyals =  28
+//var quantityUS$ = 10
+//var quantitySaudiriyals = 25
+//var totalPrice = (US$*quantityUS$)+(saudiRiyals*quantitySaudiriyals)
+//document.write("<h1>CURRENCY IN PKR</h1>")
+//document.write("Total Currency in PKR: " + totalPrice)
+
+//10)
+//var number = 10
+//var calculation = (10+5*10)/2
+//document.write(calculation)
+
+//11)
+//var currentYear = 2021
+//var birthYear = 2003
+//var calculatedAge = currentYear-birthYear
+//document.write("Current Year=" = currentYear)
+//document.write("Birth Year =" = birthYear)
+//document.write("Your Age is: " + calculatedAge)
+
+//12)
+//<h1>THE GEOMETRIZER </h1>
+//var radius = 20
+//var circumference = 2*3.142*20
+//var area = 3.142*400
+//document.write("Radius of a circle is " + radius)
+//document.write("the Circumference is " + circumference)
+//document.write("The Area is :" + area)
+
+//13)
+//var favSnack = "Chocolate Chip"
+//var currentAge = 18
+//var maxAge = 78
+//var estTime = 23.5
+//var amountDaily = 5
+//document.write("Fav Snack : " + favSnack)
+//document.write("<br>Age" + currentAge)
+//document.write("<br>Max Age " + maxAge)
+//document.write("<br>Est time " + estTime)
+//document.write("<br>Amount Daily :" +amountDaily)
+
+                        //Chapter6-9//
+//1)
+//var a = 10
+//var newValue = ++a
+//document.write(newValue)
+//var newValue = a++
+//document.write(newValue)
+//var newValue = --a
+//document.write(newValue)
+//var newValue = a--
+//document.write(newValue)
+
+//2)
+//var a = 2, b = 1;
+//var result = --a - --b + ++b + b--;
+//var firstOne = --a
+//document.write("<br>-aa:" + firstOne)
+//var secondOne = --a - --b 
+//document.write("<br>--a - --b ;" + secondOne)
+//var thirdOne = --a - --b + ++b
+//document.write("<br>--a - --b + ++b;" + thirdOne)
+//var fourthOne = --a - --b + ++b + b--
+//document.write("<br>--a - --b + ++b + b-- :" + fourthOne)
+
+//3)
+//var userName = +prompt("User Name")
+//document.write("Welcome ! "+userName)
+
+//4)
+//var number = +prompt("Number")
+//if(number= 2){
+  //  document.write("2*1=" + number*1) 
+  //  document.write("<br>2*2=" + number*2)
+  //  document.write("<br>2*3=" + number*3)
+  //  document.write("<br>2*4=" + number*4)
+  //  document.write("<br>2*5=" + number*5) 
+  //  document.write("<br>2*6=" + number*6) 
+  //  document.write("<br>2*7=" + number*7) 
+  //  document.write("<br>2*8=" + number*8) 
+   // document.write("<br>2*9=" + number*9) 
+   // document.write("<br>2*10=" + number*10)   
+//}
+//else if (numBer=5){
+  //  document.write("5*1=" + numBer*1) 
+  //  document.write("<br>5*2=" + numBer*2)
+  //  document.write("<br>5*3=" + numBer*3)
+  //  document.write("<br>5*4=" + numBer*4)
+  //  document.write("<br>5*5=" + numBer*5) 
+  //  document.write("<br>5*6=" + numBer*6) 
+  //  document.write("<br>5*7=" + numBer*7) 
+  //  document.write("<br>5*8=" + numBer*8) 
+  //  document.write("<br>5*9=" + numBer*9) 
+  //  document.write("<br>5*10=" + numBer*10)  
+//}
+//else {(numBer=5) 
+    //document.write("5*1=" + numBer*1) 
+    //document.write("<br>5*2=" + numBer*2)
+    //document.write("<br>5*3=" + numBer*3)
+    //document.write("<br>5*4=" + numBer*4)
+    //document.write("<br>5*5=" + numBer*5) 
+  //  document.write("<br>5*6=" + numBer*6) 
+//    document.write("<br>5*7=" + numBer*7) 
+    //document.write("<br>5*8=" + numBer*8) 
+   // document.write("<br>5*9=" + numBer*9) 
+ //   document.write("<br>5*10=" + numBer*10)  
+//}
+
+//6)
+//var urduMarks = +prompt("Urdu Marks")
+//var engMarks = +prompt("English Marks")
+//var mathsMarks = +prompt("Maths Marks")
+//var totalMarks = 300
+//document.write("<br>Urdu Marks="+urduMarks)
+//document.write("<br>English Marks="+engMarks)
+//document.write("<br>Maths Marks="+mathsMarks)
+//var obtainedMarks = urduMarks+engMarks+mathsMarks
+//document.write(obtainedMarks)
+//var Percentage = obtainedMarks*100/300
+//document.write("<br>Total Percentage :" + Percentage)
+
+                            //Chapter 9-12//
+//1)
+//var cityName == +prompt("Enter Your City Name")
+//if(cityName=karachi){
+//    document.write("WELCOME TO THE CITY OF LIGHTS")
+//}else{
+//    document.write("Welcome")
+//}
+
+//2)
+//var genderName = prompt("Enter Your Gender")
+//if(genderName=="male"){
+//    document.write("WELCOME SIR!")
+//
+//}
+//else if (genderName=="female"){
+//    document.write("WELCOME MADAM!")
+//}
+
+//3)
+//var trafficSignal = prompt("enter the colour of traffic signal")
+//if(trafficSignal=="red"){
+//    document.write("MUST STOP!")
+//}
+//else if (trafficSignal=="yellow"){
+//    document.write("READY TO MOVE...")
+//}
+//else if (trafficSignal=="green"){
+//    document.write("MOVE NOW")
+//}
+
+//4)
+//var fuelCheck = prompt("ENTER YOUR CAR FUEL IN LITRES")
+//if(fuelCheck<0.25){
+//    document.write("PLEASE REFILL THE FUEL")
+//}
+
+//5)
+//var a = 4;
+//if (++a === 5){
+//alert("given condition for variable a is true");
+//}
+//var b = 82;
+//if (b++ === 83){
+//alert("given condition for variable b is true");
+//}
+// var c = 12;
+//if (c++ === 13){
+//alert("condition 1 is true");
+//}
+//if (c === 13){
+//alert("condition 2 is true");
+//}
+//if (++c < 14){
+//alert("condition 3 is true");
+//}
+//if(c === 14){
+//alert("condition 4 is true");
+//}
+//var materialCost = 20000;
+//var laborCost = 2000;
+//var totalCost = materialCost + laborCost;
+//if (totalCost === laborCost + materialCost){
+//alert("The cost equals");
+//}
+//if (true){
+//alert("True");
+//}
+//if (false){
+//alert("False");
+//}
+
+//if("car" < "cat"){
+//alert("car is smaller than cat");
+//}
+
+//6)
+//var englishMarks = +prompt("ENter your english marks")
+document.write("English Marks" + englishMarks)
+//var islMarks = +prompt("ENter your Islamiat marks")
+document.write("Islamiat Marks :" + islMarks)
+var mathsMarks = +prompt("ENter your Maths marks")
+document.write("Maths Marks :" +mathsMarks)
+//var totalMarks = 300
+//document.write("Total marks" + totalMarks)
+//var Percentage = (englishMarks+islMarks+mathsMarks)*100/300
+//document.write("PErcentage:"+ Percentage)
+//if(Percentage>=80){
+//    document.write("Grade ; A-one Excellent!!!")
+//}
+//if(Percentage>=70){
+//    document.write("Grade ; A good")
+//}
+//if(Percentage>=60){
+//    document.write("Grade ; B You need to improve")
+//}
+//if(Percentage<60){
+//    document.write("Grade ; Fail Sorry")
+//}
+
